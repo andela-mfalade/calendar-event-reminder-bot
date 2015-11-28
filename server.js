438220aa6e879a515deb4b006f21d0eec2fef069
andela-mfalade <mayowa.falade@andela.co>
2015-11-28 23:02:50 +0100

    Initial Server Setup

---
+++ server.js
@@ -0,0 +1,27 @@
+var express    = require('express'),
+    bodyParser = require('body-parser'),
+    app        = express(),
+    port       = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', function(req, res) {
+    res.status(200).send("Hey, you just hit the test route");
+});
+
+app.listen(port, function() {
+    console.log(port + " is where the magic happens.")
+})
+
+app.post('/test', function(req, res, next) {
+    var userName = req.body.username;
+    var botPayLoad = {
+        text: "Hello " + userName + ". Welcome here."
+    };
+
+    if (userName !== 'slackbot') {
+        return res.status(200).json(botPayLoad);
+    } else {
+        return res.status(200).end();
+    }
+})
